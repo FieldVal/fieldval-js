06bb1d403b1fbbd3ae039e25eb80412a54cff79c
bestan <bestan93@gmail.com>
2015-03-17 14:35:13 +0200

    Fixed BasicVal.no_whitespace() check

--- src/BasicVal.js
+++ src/BasicVal.js
@@ -259,7 +259,7 @@ var BasicVal = (function(){
         no_whitespace: function(flags) {
             var check = function(value) {
                 if (/\s/.test(value)){
-                    return FieldVal.create_error(BasicVal.errors.contains_whitespace, flags, max_len);
+                    return FieldVal.create_error(BasicVal.errors.contains_whitespace, flags);
                 }
             };
             if(flags){
